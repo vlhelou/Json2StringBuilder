4a34da09c541c016760177e9746d74b6735b8afe
VASSILI.HELOU <VASSILI.HELOU@hospitalsantamarta.com.br>
2022-06-02 15:58:09 -0300

    feito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+
+// See https://aka.ms/new-console-template for more information
+
+if (args.Length!=1)
+{
+    Console.WriteLine("somente um parâmetro");
+    Environment.Exit(0);
+    
+}
+string caminho = args[0];
+
+var linhas =File.ReadLines(caminho);
+StringBuilder saida = new();
+saida.AppendLine("StringBuilder sbJson = new();");
+foreach(var linha in linhas)
+{
+    string convertido = Microsoft.CodeAnalysis.CSharp.SymbolDisplay.FormatPrimitive(linha,true, true);
+    saida.AppendLine("sbJson.AppendLine("+convertido+");");
+}
+TextCopy.ClipboardService.SetText(saida.ToString());
+Console.WriteLine(saida.ToString());
